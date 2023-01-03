6f1df386b510b10ffc07cd4f6958ff8ff9d7ae4d
Bryan <chickenonwheat@gmail.com>
2023-01-03 09:10:40 -0800

    routes added for ingre , steps, users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+    def index
+        render json: User.all
+    end
+
+    def show
+        user = User.find(params[:id])
+        render json: user
+    end
+
+    def create
+        user = User.create!(user_params)
+        render json: user, status: :created
+    end
+
+private
+
+    def user_params
+        params.permit(:name, :age, :image, :username, :password_digest)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+has_many :recipes
+has_many :favorites
+has_many :favorite_recipes, through: :favorites, source: :recipes
+has_many :reviews
+has_many :reviewed_recipes, through: :reviews, source: :recipes
+
+name, presence: true
+age, presence: true
+username, presence: true, uniqueness: true
+
+
 end
