df6d10782c0fd9d2ec5114da1de19ddad8873732
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-01-03 12:30:26 -0700

    fixed validations and seeds

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :recipe
 
-  # validates :rating, presence: true, inclusion: { in: [ 0..5 ] }
+  validates :rating, presence: true, inclusion: { in: ( 0..5 ) }
 
 end
