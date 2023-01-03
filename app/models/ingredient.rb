1a7698791b329a41ced7648a11441e8ca9f0068e
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-01-03 09:32:37 -0700

    associations and validations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,2 +1,6 @@
 class Ingredient < ApplicationRecord
+    has_many :joinirs
+    has_many :recipes, through: :joinirs
+
+    name, presence: true, uniqueness: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,14 @@
 class Recipe < ApplicationRecord
   belongs_to :user
+  has_many :favorites
+  has_many :favorited_users, through: :favorites, source: :users
+  has_many :reviews
+  has_many :reviewed_users, through: :reviews, source: :users
+  has_many :steps
+  has_many :joinirs
+  has_many :ingredients, through: :joinirs
+
+  name, presence: true
+  tags, inclusion: { in: [ "asian", "american", "italian", "vegan", "vegetarian", "mediterranean", "quick/easy", "indian", "mexican", "dessert" ] }
+
 end
