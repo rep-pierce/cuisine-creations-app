49e73fffaacc5ae0baac3baf1f0b86e2bc142fd1
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-01-03 12:13:00 -0700

    fixed steps

--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -1,16 +1,16 @@
 class StepsController < ApplicationController
     def create
-        steps = Steps.create!(steps_params)
+        steps = Step.create!(steps_params)
         render json: steps, status: :created
     end
 
     def update
-        steps = Steps.update!(steps_params)
+        steps = Step.update!(steps_params)
         render json: steps, status: :accepted
     end
 
     def delete
-        steps = Steps.find(params[:id])
+        steps = Step.find(params[:id])
         steps.destroy
         head :no_content
     end
