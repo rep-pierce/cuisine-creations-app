d329ddc10c22aa1aeb83a8a9237318a68316afbd
Bryan <chickenonwheat@gmail.com>
2023-01-03 09:13:08 -0800

    dependent destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-has_many :recipes
-has_many :favorites
+has_many :recipes, dependent: :destroy 
+has_many :favorites, dependent: :destroy 
 has_many :favorite_recipes, through: :favorites, source: :recipes
-has_many :reviews
+has_many :reviews, dependent: :destroy 
 has_many :reviewed_recipes, through: :reviews, source: :recipes
 
 name, presence: true
