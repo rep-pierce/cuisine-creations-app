6fc5b796e29df17fa147a2e6c1016c480febfe41
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-01-05 11:19:07 -0700

    create add and remove from favs

--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { useHistory } from "react-router-dom";
 
-function RecipeCard({recipe}) {
+function RecipeCard({recipe, currentUser}) {
   const history = useHistory()
   function handleNav(){
     history.push(`/recipe/${recipe.id}`)
+  }
+  function handleFav(){
+    currentUser.favorite_recipes.includes(recipe) ? <button>Remove from Favorites</button> : <button>Add to Favorites</button>
   }
   return (
     <>
@@ -14,6 +17,7 @@ function RecipeCard({recipe}) {
         <h2>{recipe.recipe_time}</h2>
         <p>{recipe.tags}</p>
       { !recipe.user ?  null : <p>Created by: {recipe.user.name}</p>}
+      { !currentUser ? null : handleFav()}
     </div>
     <br/>
     </>
